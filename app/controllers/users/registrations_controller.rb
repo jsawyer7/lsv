bd5a6425c56a2a8f8549edc8d28f9825de42b040
Abuzar <abuzarramzan4@gmail.com>
2025-04-16 05:53:05 +0500

    Implemented Auth Login/Signup with customized Flash messages

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      flash[:alert] = resource.errors.full_messages.join(', ')
+      redirect_to new_user_registration_path
+    end
+  end
+end
